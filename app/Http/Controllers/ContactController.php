5e37e1be22b66119759960aae7ee5f5a59b881fe
masum359160 <automation24@mis.prangroup.com>
2022-05-16 18:07:45 +0600

    Router Group added

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,10 +11,10 @@ use Illuminate\Support\Facades\DB;
 class ContactController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth')->only('create', 'update', 'destroy');
+    // }
 
     public function index(){
 
